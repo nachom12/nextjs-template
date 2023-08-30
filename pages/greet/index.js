40ff69a10aad36af356818427abffaa2180ab604
Juan Ignacio Medina Cruz <jmedina@bps.gub.uy>
2023-08-30 16:06:50 -0300

    Adding more generic properties to layout

--- pages/greet/index.js
+++ pages/greet/index.js
@@ -1,6 +1,8 @@
+import styles from '../../styles/pages/greet.module.scss';
+
 export default function Greet(){
   return(
-    <div className="greet">
+    <div className={styles.greet}>
       <h1><b>HI 👋😄</b> Thx for using my template</h1>
     </div>
   )
